352792b4a4171d55056315bdcb0305240d587450
Vivian DELANNOY <vivian_dlny@hotmail.com>
2024-04-22 18:05:03 +0200

    ✨ better menu icon

--- src/components/NavPannel.vue
+++ src/components/NavPannel.vue
@@ -10,7 +10,7 @@
         <div class="nav">
           <RouterLink to="/menu">
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#fffaf1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            <svg class="secondMenu" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#fffaf1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
           </RouterLink>
         </div>
 
@@ -89,5 +89,8 @@
 }
 .nav a {
   text-decoration: none;
+}
+.secondMenu{
+  transform: translateY(-98%);
 }
 </style>
